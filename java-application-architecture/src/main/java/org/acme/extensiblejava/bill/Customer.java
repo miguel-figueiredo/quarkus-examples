0ee7d4126d99e167bfb9d4611e1e209ff3224621
Miguel Figueiredo <miguel.pedro@talkdesk.com>
2022-11-03 14:06:17 +0000

    Add base code for java Application Architecture

--- java-application-architecture/src/main/java/org/acme/extensiblejava/bill/Customer.java
+++ java-application-architecture/src/main/java/org/acme/extensiblejava/bill/Customer.java
@@ -4,21 +4,21 @@ import java.util.List;
 
 public class Customer {
 	private CustomerEntityLoader loader;
-	private Integer custId;
+	private Integer customerId;
 	private Name name;
-	private List bills;
+	private List<Bill> bills;
 
 	public static Customer loadCustomer(CustomerEntityLoader loader) {
 		return loader.loadCustomer();
 	}
 
-	public Customer(Integer custId, Name name, CustomerEntityLoader loader) {
-		this.custId = custId;
+	public Customer(Integer customerId, Name name, CustomerEntityLoader loader) {
+		this.customerId = customerId;
 		this.name = name;
 		this.loader = loader;
 	}
 
-	public List getBills() {
+	public List<Bill> getBills() {
 		if (this.bills == null) {
 			this.bills = loader.loadBills();
 		}
--- java-application-architecture/src/main/java/org/acme/extensiblejava/bill/CustomerEntityLoader.java
+++ java-application-architecture/src/main/java/org/acme/extensiblejava/bill/CustomerEntityLoader.java
@@ -4,5 +4,5 @@ import java.util.List;
 
 public interface CustomerEntityLoader {
 	public Customer loadCustomer();
-	public List loadBills();
+	public List<Bill> loadBills();
 }
